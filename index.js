4c9cbf86f407c88f05583cc42f93f296544b4cde
Sarab Bedi <sarabsbedi@gmail.com>
2021-03-05 14:16:22 -0500

    Update Resume

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const menu = document.querySelector("#active");
 const barOne = document.getElementsByClassName("bar one")[0]
 const barTwo = document.getElementsByClassName("bar two")[0]
 const barThree = document.getElementsByClassName("bar three")[0]
+const currentYear = document.getElementById("year");
 
 
 toggle.addEventListener("click", ()=> {
     menu.classList.toggle("active")
-    
     barOne.classList.toggle("active")
     barTwo.classList.toggle("active")
     barThree.classList.toggle("active")
-
 })
+
+currentYear.innerHTML = new Date().getFullYear();
 
